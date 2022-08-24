cf5b4f2b2a917b23a008f55bb2fea722d48d6167
Kumpel13 <109043133+Kumpel13@users.noreply.github.com>
2022-08-24 18:16:41 +0200

    Add wizytowki.py

---
+++ wizytowki.py
@@ -0,0 +1,73 @@
+from faker import Faker
+
+faker = Faker("pl_PL")
+
+
+class BaseContact:
+    def __init__(self, name = faker.name(), phone_number = faker.phone_number(), mail = faker.email()):
+        self.name = name
+        self.phone_number = phone_number
+        self.mail = mail
+
+    def __str__(self):
+        return f'{self.name}, {self.phone_number}, {self.mail}' 
+
+    def contact(self):
+        return f"Wybieram numer {self.phone_number} i dzwonię do {self.name}"
+
+    @property
+    def label_length(self):
+        return int(len(self.name))
+
+class BusinessContact(BaseContact):
+    def __init__(self, job = faker.job(), company = faker.company(), business_phone = faker.phone_number(), *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.job = job
+        self.company = company
+        self.business_phone = business_phone
+   
+    def __str__(self):
+        return f'{self.job}, {self.company}, {self.business_phone}'
+
+    def contact(self):
+        return f"Wybieram numer {self.business_phone} i dzwonię do {self.name}"    
+
+private_contact = BaseContact()
+business_card = BusinessContact()
+
+#def create_contacts(num):
+    
+
+#contacts = input("Wybierz rodzaj wizytówki, wpisując: Base lub Business ")
+    
+#if contacts == 'Base':
+   # numbers = int(input("Podaj ilość wizytówek "))
+   # result = create_contacts(numbers)
+   # print (result)
+
+#elif contacts == 'Business':
+   # input("Podaj ilość wizytówek ")
+
+#else:
+   # print("błąd wyboru, spórbuj ponownie")
+    #exit
+
+
+
+
+
+print(private_contact)
+print(business_card)
+print('\n')
+print(private_contact.contact())
+print(business_card.contact())
+print('\n')
+print(private_contact.label_length)
+
+
+
+
+
+
+
+        
